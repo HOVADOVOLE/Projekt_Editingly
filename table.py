34022a5b45ad52f3f4439adc9f10fa5c4f35c268
dominik.vins <dominik.vins@portabo.cz>
2023-10-08 17:34:32 +0200

    upravení tabulky

--- table.py
+++ table.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
@@ -8,23 +9,23 @@ class InteractiveTableApp(App):
         # Vytvoření hlavního layoutu
         layout = BoxLayout(orientation='vertical')
 
-        # Nadpis tabulky s popisky sloupců
-        header_layout = BoxLayout(size_hint=(1, 0.1))
-        header_labels = ['#', 'Start time', 'End time', 'Duration', 'Text']
+        # Vytvoření layoutu pro tabulku s popisky sloupců
+        table_layout = GridLayout(cols=4, size_hint=(0.5, 0.7), pos_hint={'top': 0.7, 'left': 1})
+        header_labels = ['#', 'Start time', 'End time', 'Text']
         for label_text in header_labels:
-            header_label = Label(text=label_text, size_hint=(0.2, 1))
-            header_layout.add_widget(header_label)
-        layout.add_widget(header_layout)
+            header_label = Label(text=label_text, size_hint=(None, None), height=40)
+            table_layout.add_widget(header_label)
 
+
+        rowGrid = GridLayout(cols=4, size_hint=(0.5, 0.7), pos_hint={'top': 0.7, 'left': 1})
         # Vytvoření tabulky
-        for i in range(6):  # 6 řádků
-            row_layout = BoxLayout(size_hint=(1, 0.1))
-            row_layout.add_widget(Label(text=str(i+1), size_hint=(0.2, 1)))  # Číslo řádku
-            for j in range(4):  # 4 sloupce s daty
-                text_input = TextInput(multiline=False, size_hint=(0.2, 1))
-                row_layout.add_widget(text_input)
-            layout.add_widget(row_layout)
-
+        for i in range(1, 7):  # 6 řádků
+            rowGrid.add_widget(Label(text=str(i), size_hint=(None, None), height=40))  # Číslo řádku
+            for j in range(3):  # 3 sloupce s daty (bez čísla řádku)
+                text_input = TextInput(multiline=False, size_hint=(None, None), height=40)
+                rowGrid.add_widget(text_input)
+        table_layout.add_widget(rowGrid)
+        layout.add_widget(table_layout)
         return layout
 
 if __name__ == '__main__':
