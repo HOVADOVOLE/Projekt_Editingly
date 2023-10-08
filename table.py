033d2e2d75319074074f6afa53edfc785a397c80
dominik.vins <dominik.vins@portabo.cz>
2023-10-08 17:06:22 +0200

    vatvořená tabulky

--- table.py
+++ table.py
@@ -1,12 +1,31 @@
-import kivy
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-kivy.require("2.0.0")
-
-class Table(App):
+class InteractiveTableApp(App):
     def build(self):
-        return Builder.load_file('table.kv')  # Replace with your .kv file name
+        # Vytvoření hlavního layoutu
+        layout = BoxLayout(orientation='vertical')
+
+        # Nadpis tabulky s popisky sloupců
+        header_layout = BoxLayout(size_hint=(1, 0.1))
+        header_labels = ['#', 'Start time', 'End time', 'Duration', 'Text']
+        for label_text in header_labels:
+            header_label = Label(text=label_text, size_hint=(0.2, 1))
+            header_layout.add_widget(header_label)
+        layout.add_widget(header_layout)
+
+        # Vytvoření tabulky
+        for i in range(6):  # 6 řádků
+            row_layout = BoxLayout(size_hint=(1, 0.1))
+            row_layout.add_widget(Label(text=str(i+1), size_hint=(0.2, 1)))  # Číslo řádku
+            for j in range(4):  # 4 sloupce s daty
+                text_input = TextInput(multiline=False, size_hint=(0.2, 1))
+                row_layout.add_widget(text_input)
+            layout.add_widget(row_layout)
+
+        return layout
 
 if __name__ == '__main__':
-    Table().run()
+    InteractiveTableApp().run()
