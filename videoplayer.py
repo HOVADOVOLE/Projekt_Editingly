b8b88320cacb6cffc405e6c429fb68b763ac86fd
dominik.vins <dominik.vins@portabo.cz>
2023-10-14 20:53:16 +0200

    přidání možnosti volby videa

--- videoplayer.py
+++ videoplayer.py
@@ -1,6 +1,8 @@
 from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Rectangle, Color
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.popup import Popup
 
 class VideoPlayerApp(BoxLayout):
     def __init__(self):
@@ -16,17 +18,40 @@ class VideoPlayerApp(BoxLayout):
             Color(0, 0, 0, 1)  # Černá barva v RGBA
             self.rect = Rectangle(pos=self.pos, size=self.size)
 
-        video = VideoPlayer(size_hint=(1, 1))
-        video.bind(on_touch_down=self.on_touch_down)
-        self.add_widget(video)
+        self.video = VideoPlayer(size_hint=(1, 1))
+        self.video.bind(on_touch_down=self.on_touch_down)
+        self.add_widget(self.video)
 
         # Propojení velikosti a pozice čtverce s velikostí a pozicí widgetu
         self.bind(pos=self.aktualizovat_ctverec, size=self.aktualizovat_ctverec)
+
+        # Popup pro file manager
+        self.popup_file_manager = None
 
     def aktualizovat_ctverec(self, instance, hodnota):
         # Aktualizace velikosti a pozice čtverce při změně velikosti nebo pozice widgetu
         self.rect.pos = self.pos
         self.rect.size = self.size
-    def on_touch_down(self, instance):
-        if instance.source == '':
-            instance.source = 'video.mp4'
+
+    def on_touch_down(self, touch):
+        if self.video.collide_point(*touch.pos):
+            if self.video.source == '':
+                # Zdroj videa není nastaven, umožnit uživateli vybrat vlastní zdroj videa
+                self.otevrit_file_manager(None)  # Předáme None jako placeholder pro 'instance'
+
+    def otevrit_file_manager(self, instance):
+        # Vytvoření a zobrazení popupu s file managerem
+        file_chooser = FileChooserIconView(path='.', filters=['*.mp4', '*.avi', '*.mkv', '*.mov', '*.wmv'])
+        file_chooser.bind(on_submit=self.vybrat_soubor)
+        file_chooser.bind(on_cancel=self.zavrit_file_manager)
+        self.popup_file_manager = Popup(title='Vyberte video', content=file_chooser, size_hint=(0.9, 0.9))
+        self.popup_file_manager.open()
+
+    def vybrat_soubor(self, instance, selection, *args):
+        if selection:
+            self.video.source = selection[0]
+            self.video.state = 'play'  # Spustit přehrávání videa po nastavení nového zdroje
+        self.popup_file_manager.dismiss()
+
+    def zavrit_file_manager(self, instance):
+        self.popup_file_manager.dismiss()
