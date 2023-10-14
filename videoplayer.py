9274a624d0c6aa000857a0cde6d20de7584c9345
dominik.vins <dominik.vins@portabo.cz>
2023-10-14 22:26:33 +0200

    dodělaný videopřehrávač

--- videoplayer.py
+++ videoplayer.py
@@ -13,45 +13,47 @@ class VideoPlayerApp(BoxLayout):
         self.padding = 10
         self.spacing = 10
 
-        # Vytvoření černého pozadí
         with self.canvas.before:
-            Color(0, 0, 0, 1)  # Černá barva v RGBA
+            Color(0, 0, 0, 1)
             self.rect = Rectangle(pos=self.pos, size=self.size)
 
-        self.video = VideoPlayer(size_hint=(1, 1))
-        self.video.bind(on_touch_down=self.on_touch_down)
+        self.video = VideoPlayer(size_hint=(1, 1), state='pause')
         self.add_widget(self.video)
 
-        # Propojení velikosti a pozice čtverce s velikostí a pozicí widgetu
-        self.bind(pos=self.aktualizovat_ctverec, size=self.aktualizovat_ctverec)
-
-        # Popup pro file manager
+        self.bind(pos=self.update_rectangle, size=self.update_rectangle)
         self.popup_file_manager = None
 
-    def aktualizovat_ctverec(self, instance, hodnota):
-        # Aktualizace velikosti a pozice čtverce při změně velikosti nebo pozice widgetu
+        self.video.bind(on_loaded=self.on_video_loaded)
+        self.video_loaded = False
+
+    # Zajistí, že se černý obdélník přizpůsobí velikosti videa
+    def update_rectangle(self, instance, value):
         self.rect.pos = self.pos
         self.rect.size = self.size
 
-    def on_touch_down(self, touch):
-        if self.video.collide_point(*touch.pos):
-            if self.video.source == '':
-                # Zdroj videa není nastaven, umožnit uživateli vybrat vlastní zdroj videa
-                self.otevrit_file_manager(None)  # Předáme None jako placeholder pro 'instance'
+    # Pokud je video načtené, tak se při kliknutí na video otevře File Manager
+    def on_touch_up(self, touch):
+        if self.video.collide_point(*touch.pos) and not self.video_loaded and not self.video.source:
+            self.open_file_manager()
 
-    def otevrit_file_manager(self, instance):
-        # Vytvoření a zobrazení popupu s file managerem
+    # Otevření File Manageru
+    def open_file_manager(self):
         file_chooser = FileChooserIconView(path='.', filters=['*.mp4', '*.avi', '*.mkv', '*.mov', '*.wmv'])
-        file_chooser.bind(on_submit=self.vybrat_soubor)
-        file_chooser.bind(on_cancel=self.zavrit_file_manager)
+        file_chooser.bind(on_submit=self.select_file)
+        file_chooser.bind(on_cancel=self.close_file_manager)
         self.popup_file_manager = Popup(title='Vyberte video', content=file_chooser, size_hint=(0.9, 0.9))
         self.popup_file_manager.open()
 
-    def vybrat_soubor(self, instance, selection, *args):
+    # Filemanager se zavře po výběru souboru
+    def select_file(self, instance, selection, *args):
         if selection:
             self.video.source = selection[0]
-            self.video.state = 'play'  # Spustit přehrávání videa po nastavení nového zdroje
+            self.popup_file_manager.dismiss()
+
+    def close_file_manager(self, instance):
         self.popup_file_manager.dismiss()
 
-    def zavrit_file_manager(self, instance):
-        self.popup_file_manager.dismiss()
+    # Zajistí, že se File Manager otevře pouze pokud přehrávač nemá source
+    def on_video_loaded(self, instance, value):
+        self.video_loaded = True
+        self.unbind(on_touch_up=self.on_touch_up)
