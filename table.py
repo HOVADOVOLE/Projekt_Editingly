4be98f36a2bfa30c8f3eacc3008baae4aa6f5458
dominik.vins <dominik.vins@portabo.cz>
2023-11-25 17:59:50 +0100

    úprava dat v tabulce

--- table.py
+++ table.py
@@ -13,6 +13,8 @@ class InteractiveTable(RelativeLayout):
         self.size_hint = (0.75, 1)
         self.pos_hint = {'top': 0.95, 'left': 0.90}
 
+        self.row_num = 0
+
         # Vykreslení tabulky a boxu pro úpravu
         self.render_table()
         self.render_modify_box()
@@ -21,9 +23,9 @@ class InteractiveTable(RelativeLayout):
         self.add_widget(self.data_table)
         self.add_widget(self.modify_box)
     def update_values(self, *args):
-        print('Update values')
+        self.data_table.row_data[self.row_num] = (str(self.row_num+1), self.start_input.text, self.end_input.text, '00:00:00', self.text_input.text)
     def delete_row(self, *args):
-        print('Delete row')
+        self.data_table.row_data.pop(self.row_num)
     def render_table(self):
         # Inicializace MDDataTable s novým řádkem
         self.data_table = MDDataTable(
@@ -46,10 +48,10 @@ class InteractiveTable(RelativeLayout):
         )
         self.data_table.bind(on_row_press=self.select_row)
     def select_row(self, table, row):
-        row_num = int(row.index/len(table.column_data))
-        self.start_input.text = self.data_table.row_data[row_num][1]
-        self.end_input.text = self.data_table.row_data[row_num][2]
-        self.text_input.text = self.data_table.row_data[row_num][4]
+        self.row_num = int(row.index/len(table.column_data))
+        self.start_input.text = self.data_table.row_data[self.row_num][1]
+        self.end_input.text = self.data_table.row_data[self.row_num][2]
+        self.text_input.text = self.data_table.row_data[self.row_num][4]
     def render_modify_box(self):
         self.modify_box = BoxLayout(orientation='horizontal', size_hint=(0.55, 0.1), pos_hint={'left': 0.7, 'top': 0.48})
         self.button_box = BoxLayout(orientation='vertical', size_hint=(1, 1))
