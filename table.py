c82bca2d2be3549d7dd9947331beea461894db5e
dominik.vins <dominik.vins@portabo.cz>
2023-11-25 16:33:38 +0100

    úprava pro přehlednost

--- table.py
+++ table.py
@@ -13,6 +13,18 @@ class InteractiveTable(RelativeLayout):
         self.size_hint = (0.75, 1)
         self.pos_hint = {'top': 0.95, 'left': 0.90}
 
+        # Vykreslení tabulky a boxu pro úpravu
+        self.render_table()
+        self.render_modify_box()
+
+        # Přidání MDDataTable do RelativeLayout
+        self.add_widget(self.data_table)
+        self.add_widget(self.modify_box)
+    def update_values(self, *args):
+        print('Update values')
+    def delete_row(self, *args):
+        print('Delete row')
+    def render_table(self):
         # Inicializace MDDataTable s novým řádkem
         self.data_table = MDDataTable(
             size_hint=(0.55, 0.5),
@@ -32,12 +44,17 @@ class InteractiveTable(RelativeLayout):
                 ('5', '00:00:00', '00:00:00', '00:00:00', 'Text'),
             ]
         )
-        self.modify_box = BoxLayout(orientation='horizontal', size_hint=(0.55, 0.1), pos_hint={'left': 0.7, 'top': 0.48})
-        self.button_box = BoxLayout(orientation='vertical', size_hint=(1,1))
+    def render_modify_box(self):
+        self.modify_box = BoxLayout(orientation='horizontal', size_hint=(0.55, 0.1),
+                                    pos_hint={'left': 0.7, 'top': 0.48})
+        self.button_box = BoxLayout(orientation='vertical', size_hint=(1, 1))
 
-        self.modify_box.add_widget(TextInput(hint_text='Start Time', multiline=False, size_hint_y=0.4, pos_hint={'center_y':0.5}))
-        self.modify_box.add_widget(TextInput(hint_text='End Time', multiline=False, size_hint_y=0.4, pos_hint={'center_y':0.5}))
-        self.modify_box.add_widget(TextInput(hint_text='Text', multiline=False, size_hint_y=0.4, pos_hint={'center_y':0.5}))
+        self.modify_box.add_widget(
+            TextInput(hint_text='Start Time', multiline=False, size_hint_y=0.4, pos_hint={'center_y': 0.5}))
+        self.modify_box.add_widget(
+            TextInput(hint_text='End Time', multiline=False, size_hint_y=0.4, pos_hint={'center_y': 0.5}))
+        self.modify_box.add_widget(
+            TextInput(hint_text='Text', multiline=False, size_hint_y=0.4, pos_hint={'center_y': 0.5}))
         modify = Button(text='Modify')
         modify.bind(on_release=self.update_values)
         delete = Button(text='Delete')
@@ -45,15 +62,3 @@ class InteractiveTable(RelativeLayout):
         self.button_box.add_widget(modify)
         self.button_box.add_widget(delete)
         self.modify_box.add_widget(self.button_box)
-
-        # Přidání MDDataTable do RelativeLayout
-        self.add_widget(self.data_table)
-        self.add_widget(self.modify_box)
-    def update_values(self, *args):
-        print('Update values')
-    def delete_row(self, *args):
-        print('Delete row')
-    def render_table(self):
-        pass
-    def render_modify_box(self):
-        pass
